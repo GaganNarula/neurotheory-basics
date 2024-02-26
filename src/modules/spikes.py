63d67ab88b0c74874db4c1e2f1e3d1eb0b6de595
granular1986 <gagan@earkick.com>
2024-02-26 12:05:24 +0100

    Added utility for poisson spike train generation

--- src/modules/spikes.py
+++ src/modules/spikes.py
@@ -1,9 +1,10 @@
 """Methods for creating, managing, and displaying spike trains."""
 
-import numpy as np
 import datetime
 import uuid
+import numpy as np
 import scipy
+import matplotlib.pyplot as plt
 
 
 def spike_sequence_from_spike_times(
@@ -89,6 +90,22 @@ def smoothed_firing_rate(
     )
 
 
+def spiketimes_from_poisson_distribution(
+    poisson_rate: float, duration_seconds: float
+) -> list[int]:
+    """Generate a list of spike times from a Poisson distribution.
+
+    :param poisson_rate: Poisson rate
+    :type poisson_rate: float
+    :param duration_seconds: duration of the spike sequence in seconds
+    :type duration_seconds: float
+
+    :return: list of spike times
+    :rtype: list[int]
+    """
+    return np.random.poisson(poisson_rate * duration_seconds)
+
+
 class SpikeTrain:
     """Class for managing and displaying spike trains."""
 
@@ -100,6 +117,7 @@ class SpikeTrain:
         sampling_period: float,
         neuron_id: int | str = None,
         recording_datetime: str | datetime.datetime = None,
+        spike_train_metadata: dict = None,
     ) -> None:
         """Initialize a SpikeTrain object.
 
@@ -111,7 +129,22 @@ class SpikeTrain:
         :type duration_samples: int
         :param sampling_period: sampling period in seconds
         :type sampling_period: float
+        :param neuron_id: neuron ID
+        :type neuron_id: int | str, optional
+        :param recording_datetime: recording datetime
+        :type recording_datetime: str | datetime.datetime, optional
+        :param spike_train_metadata: metadata for the spike train
+        :type spike_train_metadata: dict, optional
         """
+        assert isinstance(
+            spike_times, (list, np.ndarray)
+        ), "spike_times must be a list or numpy array."
+        assert isinstance(
+            duration_seconds, (int, float)
+        ), "duration_seconds must be an integer or float."
+        assert isinstance(duration_samples, int), "duration_samples must be an integer."
+        assert isinstance(sampling_period, float), "sampling_period must be a float."
+
         self.spike_times = spike_times
         self.spike_sequence = spike_sequence_from_spike_times(
             spike_times, sampling_period, duration_samples
@@ -121,6 +154,56 @@ class SpikeTrain:
         self.sampling_period = sampling_period
         self.neuron_id = neuron_id
         self.recording_datetime = recording_datetime
+
+    @classmethod
+    def from_poisson(
+        cls,
+        firing_rate: float | list[float] | np.ndarray,
+        duration_seconds: float,
+        sampling_period: float,
+        neuron_id: int | str = None,
+        recording_datetime: str | datetime.datetime = None,
+        spike_train_metadata: dict = None,
+    ) -> "SpikeTrain":
+        """Create a spike train from a Poisson distribution.
+
+        :param firing_rate: the firing rate of the spike train. If scalar, it is the average of the whole spike sequence. If
+            list, each value in the list is the firing rate for a small period of time, period duration = duration_seconds.
+        :type firing_rate: float | list[float]
+        :param duration_seconds: duration of the spike sequence in seconds, or it is the duration of a small period of time
+        :type duration_seconds: float
+        :param neuron_id: neuron ID
+        :type neuron_id: int | str, optional
+        :param recording_datetime: recording datetime
+        :type recording_datetime: str | datetime.datetime, optional
+        :param spike_train_metadata: metadata for the spike train
+        :type spike_train_metadata: dict, optional
+
+        :return: spike train
+        :rtype: SpikeTrain
+        """
+        if isinstance(firing_rate, (list, np.ndarray)):
+            spike_times = []
+            for rate in firing_rate:
+                spike_times.append(
+                    spiketimes_from_poisson_distribution(rate, duration_seconds)
+                )
+        else:
+            spike_times = spiketimes_from_poisson_distribution(
+                firing_rate, duration_seconds
+            )
+        # recompute the duration in samples
+        duration_samples = len(spike_times)
+        duration_seconds = duration_samples * sampling_period
+        return cls(
+            np.array(spike_times),
+            duration_seconds,
+            duration_samples,
+            sampling_period,
+            neuron_id,
+            recording_datetime,
+            spike_train_metadata,
+        )
 
     @property
     def num_spikes(self) -> int:
@@ -138,8 +221,6 @@ class SpikeTrain:
 
     def plot(self) -> None:
         """Plot the spike train."""
-        import matplotlib.pyplot as plt
-
         plt.eventplot(self.spike_times)
         plt.xlabel("Time (s)")
         plt.ylabel("Neuron ID = " + str(self.neuron_id))
@@ -155,20 +236,27 @@ class SpikeTrainCollection(dict):
         self,
         spike_trains: list[SpikeTrain],
         handle_missing_neuron_id: str = "raise",
+        collection_metadata: dict = None,
     ) -> None:
         """Initialize a SpikeTrainCollection object.
 
         :param spike_trains: list of spike trains
         :type spike_trains: list[SpikeTrain]
-        :param duration_seconds: duration of the spike sequence in seconds
-        :type duration_seconds: float
-        :param duration_samples: duration of the spike sequence in samples
-        :type duration_samples: int
-        :param sampling_period: sampling period in seconds
-        :type sampling_period: float
+        :param handle_missing_neuron_id: how to handle missing neuron ID and recording datetime
+        :type handle_missing_neuron_id: str, optional
+        :param collection_metadata: metadata for the collection
+        :type collection_metadata: dict, optional
         """
+        assert isinstance(spike_trains, list), "spike_trains must be a list."
+        assert isinstance(
+            handle_missing_neuron_id, str
+        ), "handle_missing_neuron_id must be a string, one of 'raise' or 'assign'."
+        assert collection_metadata is None or isinstance(
+            collection_metadata, dict
+        ), "collection_metadata must be a dictionary."
+
         self.spike_trains: dict[str, SpikeTrain] = {}
-        for i, spike_train in enumerate(spike_trains):
+        for _, spike_train in enumerate(spike_trains):
 
             if spike_train.neuron_id is None or spike_train.recording_datetime is None:
 
@@ -191,8 +279,10 @@ class SpikeTrainCollection(dict):
 
             self.spike_trains[index_name] = spike_train
 
+        self.collection_metadata = collection_metadata
 
-class MultiNeuronTrial(SpikeTrainCollection):
+
+class TrialCollection(SpikeTrainCollection):
 
     def __init__(
         self,
@@ -201,7 +291,9 @@ class MultiNeuronTrial(SpikeTrainCollection):
         duration_samples: int,
         sampling_period: float,
     ) -> None:
-        """Initialize a MultiNeuronTrial object.
+        """Initialize a TrialCollection object. A trial collection is a set of spike trains
+        from multiple neurons or the same neuron recorded multiple times. The spike trains are
+        indexed by neuron ID and recording datetime.
 
         :param spike_trains: list of spike trains
         :type spike_trains: list[SpikeTrain]
@@ -217,6 +309,18 @@ class MultiNeuronTrial(SpikeTrainCollection):
         self.duration_samples = duration_samples
         self.sampling_period = sampling_period
 
+        # assert that all spike trains have the same duration and sampling period
+        for _, spike_train in self.spike_trains.items():
+            assert (
+                spike_train.duration_seconds == self.duration_seconds
+            ), "All spike trains must have the same duration in seconds."
+            assert (
+                spike_train.duration_samples == self.duration_samples
+            ), "All spike trains must have the same duration in samples."
+            assert (
+                spike_train.sampling_period == self.sampling_period
+            ), "All spike trains must have the same sampling period."
+
     def to_matrix(self) -> np.ndarray:
         """Return a matrix representation of the spike trains."""
         # stack all spike sequences
@@ -225,8 +329,18 @@ class MultiNeuronTrial(SpikeTrainCollection):
         )
         return spike_sequences
 
-    def average_firing_rate(self):
+    def average_firing_rate(self) -> np.ndarray:
         """Return the mean firing rate across all neurons. Mean is only taken across
-        neurons."""
+        neuron axis, preserving time axis.
+        """
         spike_matrix = self.to_matrix()
         return np.mean(spike_matrix, axis=0)
+
+    def raster_plot(self) -> None:
+        """Plot the raster plot of the spike trains."""
+
+        for i, spike_train in enumerate(self.spike_trains.values()):
+            plt.eventplot(spike_train.spike_times, lineoffsets=i)
+        plt.xlabel("Time (s)")
+        plt.ylabel("Neuron ID")
+        plt.title("Raster Plot")
